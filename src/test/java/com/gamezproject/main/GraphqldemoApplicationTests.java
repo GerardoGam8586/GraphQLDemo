b2339f238b115b6f6fbf29305d68db5b9f670076
ggamez <ggamez@freedomdebtrelief.com>
2021-12-03 18:25:03 -0600

    adding test case

--- src/test/java/com/gamezproject/main/GraphqldemoApplicationTests.java
+++ src/test/java/com/gamezproject/main/GraphqldemoApplicationTests.java
@@ -1,13 +1,41 @@
 package com.gamezproject.main;
 
+import com.graphql.spring.boot.test.GraphQLResponse;
+import com.graphql.spring.boot.test.GraphQLTest;
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 
-@SpringBootTest
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = GraphqldemoApplication.class)
 class GraphqldemoApplicationTests {
 
+	@Autowired
+	private GraphQLTestTemplate graphQLTestTemplate;
+
+	private static final String GRAPHQL_QUERY_RESQUEST = "request/recentPosts.query";
+	private static final String GRAPHQL_QUERY_RESPONSE = "response/recentPosts.json";
+
 	@Test
-	void contextLoads() {
+	void graphQlTest() throws IOException {
+		String testName = "recentPosts";
+		GraphQLResponse graphQLResponse = graphQLTestTemplate.postForResource(String.format(GRAPHQL_QUERY_RESQUEST, testName));
+
+		Assertions.assertTrue(graphQLResponse.getStatusCode().is2xxSuccessful());
+
+		String expectedResponseBody = readGraphQlJson(String.format(GRAPHQL_QUERY_RESPONSE, testName));
+
+		Assertions.assertEquals(expectedResponseBody, graphQLResponse.getRawResponse().getBody());
+	}
+
+	private String readGraphQlJson(String location) throws IOException {
+		return IOUtils.toString(new ClassPathResource(location).getInputStream(), StandardCharsets.UTF_8);
 	}
 
 }
